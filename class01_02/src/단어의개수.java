958447aacd6da9b9743231b12a1ba3ec8914cc0d
yeyounging <133792082+yeyounging@users.noreply.github.com>
2023-07-17 20:42:43 +0900

    단어의개수

---
+++ class01_02/src/단어의개수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine().trim();
+
+        if(input.isEmpty()==false) {
+            String[] arr = input.split(" ");
+            System.out.println(arr.length);
+        }
+        else System.out.println(0);
+    }
+}
