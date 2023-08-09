d092d379c9b08cf9604bcd0f15607fb0dc68273b
yeyounging <133792082+yeyounging@users.noreply.github.com>
2023-08-09 16:19:49 +0900

    timeover2

--- class01_02/src/Main.java
+++ class01_02/src/Main.java
@@ -1,34 +1,33 @@
-import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    public static void main (String args[]){
-        Scanner in = new Scanner(System.in);
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n_arr = in.nextInt();
+        int n_arr = Integer.parseInt(br.readLine());
 
         StringBuilder sb= new StringBuilder();
-        sb.append("@");
-        for(int i=0;i<n_arr;i++){
-            sb.append(in.next());
-            sb.append("@");
-        }
+        sb.append(" ");
+        sb.append(br.readLine());
+        sb.append(" ");
 
-        int n_input = in.nextInt();
-        String[] input = new String[n_input];
-
-        for(int i=0;i<n_input;i++){
-            input[i]=in.next();
-        }
+        int n_input = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
 
        String arr = String.valueOf(sb);
 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         for(int i=0;i<n_input;i++){
-            String temp = "@"+input[i]+"@";
+            String temp = " "+input[i]+" ";
 
             if(arr.contains(temp) == true )
-                System.out.println("1");
-            else System.out.println("0");
+                bw.write("1\n");
+            else
+                bw.write("0\n");
         }
+        bw.close();
 
     }
 
