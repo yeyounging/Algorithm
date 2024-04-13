001772887dccbd08ea1504f537dc0eb050adcc38
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-04-13 16:33:48 +0900

    1436

---
+++ class01_02/src/영화감독숌.java
@@ -0,0 +1,25 @@
+//더 효율적인 방법 생각하기.
+
+import java.io.*;
+import java.util.*;
+
+public class 영화감독 {
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int cnt =1;
+        int i = 666;
+        while(cnt != num){
+            i++;
+            if(String.valueOf(i).contains("666")) cnt++;
+        }
+        bw.write(String.valueOf(i));
+        bw.flush();
+        bw.close();
+
+    }
+
+
+}
