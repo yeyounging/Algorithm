f47b814ebc401ae820017626135b686bffd29791
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-04-16 02:03:05 +0900

    1920

---
+++ class01_02/src/나이순정렬.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Member {
+    int age;
+    String name;
+}
+public class 나이순정렬 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        Member[] members = new Member[num];
+
+        for(int i =0;i<num;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            members[i] = new Member();
+            members[i].age = Integer.parseInt(st.nextToken());
+            members[i].name = st.nextToken();
+        }
+
+        Arrays.sort(members, (a,b)-> a.age - b.age);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i =0;i<num;i++){
+            bw.write(String.valueOf(members[i].age) + " " + members[i].name);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
