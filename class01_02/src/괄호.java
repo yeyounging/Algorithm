5254ef06d0dbefe6f04b259e8eb640722c75b675
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-04-16 22:23:16 +0900

    1920

---
+++ class01_02/src/괄호.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class 괄호 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i =0; i < num ; i++){
+            char input[] = br.readLine().toCharArray();
+            int cnt = 0; // '(' 카운트
+
+            for(int j = 0;j<input.length;j++){
+                char temp = input[j];
+
+                if(temp == '(') cnt ++;
+                else cnt --;
+
+                if (cnt < 0 ) break;
+            }
+
+            if(cnt == 0) sb.append("YES\n");
+            else sb.append("NO\n");
+        }
+        System.out.println(sb);
+    }
+}
