f0fedde5614e44d8295a8df8e064571114c189b0
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-04-27 17:35:23 +0900

    카드2

---
+++ class01_02/src/최대공약수와최소공배수.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class 최대공약수와최소공배수{
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb = new StringBuilder();
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int answer1 =1;
+        int answer2=n1*n2;
+
+        int smallN, largeN = 0;
+        if(n1>n2){
+            smallN = n2;
+            largeN = n1;
+        }
+        else{
+            smallN = n1;
+            largeN = n2;
+        }
+
+        if(largeN%smallN==0){
+            sb.append(smallN+" "+largeN);
+            System.out.println(sb);
+        }
+        else{       //최대공약수 구하기
+            for(int i = smallN/2; i>1; i--){
+                if(largeN % i == 0 && smallN % i == 0){
+                    answer1 = i;
+                    break;
+                }
+            }
+
+            //최소공배수 구하기
+            for(int i= 2 ; i<smallN ; i++ ){
+                if(largeN*i % smallN == 0) {
+                    answer2 = largeN*i;
+                    break;
+                }
+            }
+
+            sb.append(answer1+" "+answer2);
+            System.out.println(sb);
+
+        }
+    }
+}
---
+++ class01_02/src/카드2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class 카드2{
+    public static void main (String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+        boolean mode = true; //첫번째 수가 버려지는 경우
+        int firstN = 1;
+        int interval = 1;
+
+        for( int i=input ; i!=1; i=i/2 ) {
+            if(mode == true) firstN =  firstN+interval;
+            interval *=2 ;
+            if(i % 2 != 0){
+                if(mode == false) i++;
+                mode=!mode;
+            }
+        }
+        bw.write(String.valueOf(firstN));
+        bw.flush();
+        bw.close();
+    }
+}
