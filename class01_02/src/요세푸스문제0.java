5cbd68a85f70d604387e0934aff4a4b1a0f3017c
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-05-01 23:57:20 +0900

    11866

--- class01_02/src/요세푸스문제0.java
+++ class01_02/src/요세푸스문제0.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Main{
+class 요세푸스문제0{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,4 +32,4 @@ class Main{
         bw.flush();
         bw.close();
     }
-        }
+}
