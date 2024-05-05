40357a742ae588fc42e16bf1eeeb407109dbe627
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-05-06 03:24:48 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 140 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int size = Integer.parseInt(st.nextToken());
+        int gap =  Integer.parseInt(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        LinkedList<Integer> linked = new LinkedList<>();
+
+        for(int i=1;i<=size;i++){
+            linked.add(i);
+        }
+        bw.write("<");
+
+        for(int i=gap-1;linked.size()>1;i+=gap-1){
+            if(i>= linked.size()) {
+                i = i % linked.size();
+            }
+            bw.write(linked.remove(i)+", ");
+        }
+        bw.write(linked.poll() +">");
+        bw.flush();
+        bw.close();
+    }
+}
