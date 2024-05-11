b4b80521ef6ef1e3a9c19933da4a4d200da071fd
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-05-11 14:10:10 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,19 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-//재귀로 구현
+//재귀+인접행렬로 구현
 
 public class Main {
     static int num;
     static int cnt=0;
-    static boolean [][] adj = new boolean[100][100];
-    static boolean [] visited = new boolean[100];
+    static boolean [][] adj;
+    static boolean [] visited;
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         num = Integer.parseInt((br.readLine()));
         int num2 = Integer.parseInt((br.readLine()));
+        
+        adj = new boolean[num][num];
+        visited = new boolean[num];
 
         for(int i =0;i<num2;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -39,6 +42,4 @@ public class Main {
             }
         }
     }
-
-
 }
