01273e62ceac87e53872c2a9db596d20b5394dae
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-05-26 14:28:22 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 32372 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -6,35 +6,28 @@ public class Main {
     static int[] arr;
     static int[] contain;
     static int k,n;
+    static BufferedWriter bw;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         k = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
 
-        //1~k까지 n개
-        //길이가 n -> print, return
-
         arr = new int[n];
         contain = new int[k+1];
 
-        for(int i=1;i<=k;i++){
-            arr[0] = i;
-            contain[i] =1;
-            yap(1);
-            contain[i] = 0;
-        }
+        yap(0);
 
         bw.flush();
         bw.close();
     }
 
-    static void yap(int index){
+    static void yap(int index) throws IOException {
         if(index==n) {
-            for(int i : arr) System.out.print(i+" ");
-            System.out.print("\n");
+            for(int i : arr) bw.write(i+" ");
+            bw.newLine();
             contain[arr[index-1]] =0;
             return;
         }
